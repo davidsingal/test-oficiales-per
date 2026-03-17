138b16cb7c5fd8ad92b81349b5840f5cf1e7063f
David Inga <david@transparenc.earth>
2026-03-17 16:08:52 +0100

    feat: implement QuestionItem component with radio button answers and create Label component

--- src/components/question-item.tsx
+++ src/components/question-item.tsx
@@ -1,6 +1,11 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import type { FC } from "react";
-import type { Question } from "@/types/payload-types";
+import type { Answer, Question } from "@/types/payload-types";
+
+const isAnswer = (answer: number | Answer): answer is Answer =>
+  typeof answer === "object" && answer !== null;
 
 const QuestionItem: FC<{ data: Question }> = ({ data }) => {
   return (
@@ -11,7 +16,14 @@ const QuestionItem: FC<{ data: Question }> = ({ data }) => {
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <p></p>
+        <RadioGroup className="space-y-2">
+          {data.answers?.filter(isAnswer).map((answer) => (
+            <div className="flex items-center gap-2" key={answer.id}>
+              <RadioGroupItem value={answer.answerId} id={answer.answerId} />
+              <Label htmlFor={answer.answerId}>{answer.answerText}</Label>
+            </div>
+          ))}
+        </RadioGroup>
       </CardContent>
     </Card>
   );
